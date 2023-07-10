437131b7801303c399d0817418898a421f74c01d
ducmngx <nminhduc2903@gmail.com>
2023-07-10 16:17:44 -0400

    Add SQL code

---
+++ sql_scripts/tables/TBL_DATETIME_DIM.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS datetime_dimension(
+	service_date DATE NOT NULL,
+	service_week VARCHAR(50) NOT NULL,
+	service_year INT NOT NULL,
+	service_yearmonth DATE NOT NULL,
+    service_quarter INT NOT NULL,
+	PRIMARY KEY (service_date) 
+);
---
+++ sql_scripts/tables/TBL_JOBPOST_FACTS.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS jobPost_facts(
+    jobPostingID INT NOT NULL
+    companyID INT NOT NULL,
+    jobTitle VARCHAR(20) NOT NULL,
+    expLevel VARCHAR(20) NOT NULL,
+    workRemoteAllowed BIT NOT NULL,
+    jobLocation VARCHAR(50) NOT NULL,
+    requirement VARCHAR(100) NOT NULL,
+    applyLink VARCHAR(100) NOT NULL,
+    jobState BIT NOT NULL,
+    openDate DATE NOT NULL,
+    closeDate DATE NULL,
+    PRIMARY KEY (jobPostingID),
+    FOREIGN KEY (companyID) REFERENCES company_dimension (companyID)
+    FOREIGN KEY (openDate) REFERENCES datetime_dimension (service_date),
+    FOREIGN KEY (closeDate) REFERENCES datetime_dimension (service_date)
+);
