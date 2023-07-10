10b90ccebed60e4d7e84b7a47ef3e97b051e4eb5
Duc (Aaron) M. Nguyen <43735502+ducmngx@users.noreply.github.com>
2023-07-10 16:19:45 -0400

    Update TBL_DATETIME_DIM.sql

--- sql_scripts/tables/TBL_DATETIME_DIM.sql
+++ sql_scripts/tables/TBL_DATETIME_DIM.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS datetime_dimension(
 	service_week VARCHAR(50) NOT NULL,
 	service_year INT NOT NULL,
 	service_yearmonth DATE NOT NULL,
-    service_quarter INT NOT NULL,
+        service_quarter INT NOT NULL,
 	PRIMARY KEY (service_date) 
 );
