b060111e640e3b609a99af6f5ebdb60c20c4c111
ducmngx <nminhduc2903@gmail.com>
2023-07-14 15:35:01 -0400

    add SQL table

--- sql_scripts/tables/TBL_COMPANY_DIM.sql
+++ sql_scripts/tables/TBL_COMPANY_DIM.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS company_dimension {
+CREATE TABLE IF NOT EXISTS company_dimension (
     companyID INT NOT NULL,
     cName VARCHAR(20) NOT NULL,
     cHQLocation VARCHAR(50) NOT NULL,
     cWebsite VARCHAR(100) NULL,
     cField VARCHAR(50) NULL,
     PRIMARY KEY (companyID)
-}
+);
--- sql_scripts/tables/TBL_JOBPOST_FACTS.sql
+++ sql_scripts/tables/TBL_JOBPOST_FACTS.sql
@@ -1,5 +1,6 @@
+-- Active: 1687149754613@@127.0.0.1@3306@LinkedInDB
 CREATE TABLE IF NOT EXISTS jobPost_facts(
-    jobPostingID INT NOT NULL
+    jobPostingID INT NOT NULL,
     companyID INT NOT NULL,
     jobTitle VARCHAR(20) NOT NULL,
     expLevel VARCHAR(20) NOT NULL,
@@ -11,7 +12,7 @@ CREATE TABLE IF NOT EXISTS jobPost_facts(
     openDate DATE NOT NULL,
     closeDate DATE NULL,
     PRIMARY KEY (jobPostingID),
-    FOREIGN KEY (companyID) REFERENCES company_dimension (companyID)
+    FOREIGN KEY (companyID) REFERENCES company_dimension (companyID),
     FOREIGN KEY (openDate) REFERENCES datetime_dimension (service_date),
     FOREIGN KEY (closeDate) REFERENCES datetime_dimension (service_date)
 );
